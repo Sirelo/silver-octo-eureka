ee456908960d9d2691350c99d8d8becf6e6732a9
Grisha <RUen3@proton.me>
2025-11-29 10:38:08 +0300

    Update main.py and move UI file to Python class

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
 import sys
 import random
-
-from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QPainter, QColor, QPen, QBrush
 
+from ui_file import Ui_MainWindow
+
+
 class Circle:
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, color: QColor):
         self.x = x
         self.y = y
         self.radius = radius
+        self.color = color
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
         self.circles = []
 
-        self.draw_button.clicked.connect(self.add_circle)
+        self.ui.draw_button.clicked.connect(self.add_circle)
 
     def add_circle(self):
-        radius = random.randint(10, 80)
-        x = random.randint(radius, self.width() - radius)
-        y = random.randint(radius, self.height() - radius)
+        radius = random.randint(20, 80)
 
-        self.circles.append(Circle(x, y, radius))
+        w = self.ui.centralwidget.width()
+        h = self.ui.centralwidget.height()
+
+        x = random.randint(radius, w - radius)
+        y = random.randint(radius, h - radius - 100)
+
+        color = QColor(
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255)
+        )
+
+        self.circles.append(Circle(x, y, radius, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
 
-        painter.setBrush(QBrush(QColor(255, 255, 0)))
-        painter.setPen(QPen(QColor(0, 0, 0), 1))
-
         for circle in self.circles:
+            painter.setBrush(QBrush(circle.color))
+            painter.setPen(QPen(QColor(0, 0, 0), 2))
+
             painter.drawEllipse(
                 circle.x - circle.radius,
                 circle.y - circle.radius,
@@ -44,8 +58,8 @@ class MyWidget(QMainWindow):
             )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
+    window = MyWidget()
+    window.show()
     sys.exit(app.exec())
